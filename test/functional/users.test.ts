e0a0401b05bf51c4c88bf4a5fbb15c70afb9c735
Luan Freitas <luannr16@gmail.com>
2022-05-22 17:05:05 -0300

    :sparkles: p03

--- test/functional/users.test.ts
+++ test/functional/users.test.ts
@@ -1,11 +1,12 @@
 import { User } from '@src/models/user';
+import AuthService from '@src/services/auth';
 
 describe('Users functional testes', () => {
   beforeEach(async () => {
     await User.deleteMany({});
   });
   describe('When creating a new user', () => {
-    it('should successfully create a new user', async () => {
+    it('Should successfully create a new user with encrypted password', async () => {
       const newUser = {
         name: 'John Doe',
         email: 'john@email.com',
@@ -14,7 +15,14 @@ describe('Users functional testes', () => {
       const response = await global.testRequest.post('/users').send(newUser);
 
       expect(response.status).toBe(201);
-      expect(response.body).toEqual(expect.objectContaining(newUser));
+      await expect(
+        AuthService.comparePasswords(newUser.password, response.body.password)
+      ).resolves.toBeTruthy();
+      expect(response.body).toEqual(
+        expect.objectContaining( {
+          ...newUser, 
+          ...{ password: expect.any(String) },
+        }));
     });
 
     it('Should return 422 when there is a validation error', async () => {
@@ -32,19 +40,19 @@ describe('Users functional testes', () => {
     });
 
     it('Should return 409 when the email already exists', async () => {
-        const newUser = {
-          name: 'John Doe',
-          email: 'john@mail.com',
-          password: '1234',
-        };
-        await global.testRequest.post('/users').send(newUser);
-        const response = await global.testRequest.post('/users').send(newUser);
-  
-        expect(response.status).toBe(409);
-        expect(response.body).toEqual({
-          code: 409,
-          error: 'User validation failed: email: already exists in the database.',
-        });
+      const newUser = {
+        name: 'John Doe',
+        email: 'john@mail.com',
+        password: '1234',
+      };
+      await global.testRequest.post('/users').send(newUser);
+      const response = await global.testRequest.post('/users').send(newUser);
+
+      expect(response.status).toBe(409);
+      expect(response.body).toEqual({
+        code: 409,
+        error: 'User validation failed: email: already exists in the database.',
+      });
     });
   });
 });
