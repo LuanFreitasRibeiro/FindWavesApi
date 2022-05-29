011ca2cf08d72d5dfc21f23a8ee370fcf66da024
Luan Freitas <luannr16@gmail.com>
2022-05-28 21:12:23 -0300

    :sparkles: p04

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,18 +1,26 @@
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import config from 'config';
 
 export default class AuthService {
+  public static async hasPassword(
+    password: string,
+    salt = 10
+  ): Promise<string> {
+    return await bcrypt.hash(password, salt);
+  }
 
-    public static async hasPassword(
-        password: string,
-        salt = 10
-    ): Promise<string> {
-        return await bcrypt.hash(password, salt);
-    }
+  public static async comparePasswords(
+    password: string,
+    hashedPassword: string
+  ): Promise<boolean> {
+    return await bcrypt.compare(password, hashedPassword);
+  }
 
-    public static async comparePasswords(
-        password: string,
-        hashedPassword: string
-    ): Promise<boolean> {
-        return await bcrypt.compare(password, hashedPassword);
-    }
+  public static generateToken(payload: object): string{
+    
+    return jwt.sign(payload, 'test', {
+      expiresIn: 10000,
+    });
+  }
 }
--- test/functional/users.test.ts
+++ test/functional/users.test.ts
@@ -19,10 +19,11 @@ describe('Users functional testes', () => {
         AuthService.comparePasswords(newUser.password, response.body.password)
       ).resolves.toBeTruthy();
       expect(response.body).toEqual(
-        expect.objectContaining( {
-          ...newUser, 
+        expect.objectContaining({
+          ...newUser,
           ...{ password: expect.any(String) },
-        }));
+        })
+      );
     });
 
     it('Should return 422 when there is a validation error', async () => {
@@ -53,6 +54,48 @@ describe('Users functional testes', () => {
         code: 409,
         error: 'User validation failed: email: already exists in the database.',
       });
+    });
+  });
+
+  describe('When authenticating a user', () => {
+    it('should generate a token for a valid user', async () => {
+      const newUser = {
+        name: 'John Doe',
+        email: 'john@email.com',
+        password: '1234',
+      };
+
+      await new User(newUser).save();
+      const response = await global.testRequest
+        .post('/users/authenticate')
+        .send(newUser)
+        .send({ email: newUser.email, password: newUser.password });
+
+      expect(response.body).toEqual(
+        expect.objectContaining({ token: expect.any(String) })
+      );
+    });
+
+    it('Should return UNAUTHORIZED if the user the given email is not found', async() => {
+      const response = await global.testRequest
+        .post('/users/authenticate')
+        .send({ email: 'some-email@mail.com', password: '1234'})
+
+      expect(response.status).toBe(401);
+    });
+
+    it('Should return UNAUTHORIZED if the user is found but the password does not match', async () => {
+      const newUser = {
+        name: 'John Doe',
+        email: 'john@mail.com',
+        password: '1234',
+      };
+      await new User(newUser).save();
+      const response = await global.testRequest
+        .post('/users/authenticate')
+        .send({ email: newUser.email, password: 'different password' });
+
+      expect(response.status).toBe(401);
     });
   });
 });
